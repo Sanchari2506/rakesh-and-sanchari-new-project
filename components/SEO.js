e4d39ee98081a876dd6831549392ff57f2631d55
Sanchari2506 <sancharirakshit2709@gmail.com>
2026-04-11 23:35:45 +0530

    article seo

--- components/SEO.js
+++ components/SEO.js
@@ -14,16 +14,33 @@ export default function SEO({ title, description, image, url, contentType }) {
       {/* Open Graph */}
       <meta property="og:type" content={contentType} key="og:type" />
       <meta property="og:title" content={title} key="og:title" />
-      <meta property="og:description" content={description} key="og:description" />
+      <meta
+        property="og:description"
+        content={description}
+        key="og:description"
+      />
       <meta property="og:image" content={image} key="og:image" />
       <meta property="og:image:width" content="1200" key="og:image:width" />
       <meta property="og:image:height" content="630" key="og:image:height" />
       <meta property="og:url" content={url} key="og:url" />
+      <meta
+        property="og:image:secure_url"
+        content={image}
+        key="og:image:secure_url"
+      />
 
       {/* Twitter */}
-      <meta name="twitter:card" content="summary_large_image" key="twitter:card" />
+      <meta
+        name="twitter:card"
+        content="summary_large_image"
+        key="twitter:card"
+      />
       <meta name="twitter:title" content={title} key="twitter:title" />
-      <meta name="twitter:description" content={description} key="twitter:description" />
+      <meta
+        name="twitter:description"
+        content={description}
+        key="twitter:description"
+      />
       <meta name="twitter:image" content={image} key="twitter:image" />
     </Head>
   );
--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  async headers() {
+    return [
+      {
+        source: "/images/:path*",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "image/png",
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
